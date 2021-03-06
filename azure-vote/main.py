b4e0f5c53d7ff1810c9d5bab4d1715d6925470c9
ricarfantinelli <45544590+ricarfantinelli@users.noreply.github.com>
2021-03-06 13:57:10 +0100

    Changes to avoid creating unnecessary log/traces

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -24,37 +24,40 @@ from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 from applicationinsights.flask.ext import AppInsights
 from applicationinsights import TelemetryClient
 
+
+app = Flask(__name__)
+
+# Load configurations from environment or config file
+app.config.from_pyfile('config_file.cfg')
+
+print("Instrument id is: {0}".format(app.config['INSTRUMENTATION_KEY_FULL']))
 # Logging
 logger = logging.getLogger(__name__)
 logger.addHandler(AzureLogHandler(
-    connection_string='InstrumentationKey=9db51273-db0c-4c77-be9e-bcfef72dfdfa')
+    connection_string=app.config['INSTRUMENTATION_KEY_FULL'])
 )
 
 # Metrics
 exporter = metrics_exporter.new_metrics_exporter(
                enable_standard_metrics=True,
-               connection_string='InstrumentationKey=9db51273-db0c-4c77-be9e-bcfef72dfdfa')
+               connection_string=app.config['INSTRUMENTATION_KEY_FULL'])
 
 # Tracing
 tracer = Tracer(
     exporter=AzureExporter(
-        connection_string='InstrumentationKey=9db51273-db0c-4c77-be9e-bcfef72dfdfa'),
+        connection_string=app.config['INSTRUMENTATION_KEY_FULL']),
     sampler=ProbabilitySampler(1.0),
 )
 
-app = Flask(__name__)
-#appinsights = AppInsights(app)
 
 
 # Requests
 middleware = FlaskMiddleware(
     app,
-    exporter=AzureExporter(connection_string='InstrumentationKey=9db51273-db0c-4c77-be9e-bcfef72dfdfa'),
+    exporter=AzureExporter(connection_string=app.config['INSTRUMENTATION_KEY_FULL']),
     sampler=ProbabilitySampler(rate=1.0),
 )
 
-# Load configurations from environment or config file
-app.config.from_pyfile('config_file.cfg')
 
 if ("VOTE1VALUE" in os.environ and os.environ['VOTE1VALUE']):
     button1 = os.environ['VOTE1VALUE']
@@ -83,7 +86,7 @@ if not r.get(button1): r.set(button1,0)
 if not r.get(button2): r.set(button2,0)
 
 # Custom events
-app.config['APPINSIGHTS_INSTRUMENTATIONKEY'] = 'InstrumentationKey=9db51273-db0c-4c77-be9e-bcfef72dfdfa'
+#app.config['APPINSIGHTS_INSTRUMENTATIONKEY'] = 'InstrumentationKey=9db51273-db0c-4c77-be9e-bcfef72dfdfa'
 # log requests, traces and exceptions to the Application Insights service
 appinsights = AppInsights(app)
 
@@ -115,21 +118,20 @@ def index():
 
             r.set(button2,0)
 
-            tc = TelemetryClient('9db51273-db0c-4c77-be9e-bcfef72dfdfa')
-            tc.track_event('Cats votes')
-            tc.track_event('Dogs votes')
-            tc.flush()
+            try:
+                if int(vote1) > 0:
+                    properties = {'custom_dimensions': {'Cats Vote': vote1}}
+                    # TODO: use logger object to log cat vote
+                    with tracer.span(name=vote1) as span:
+                        logger.warning('cat vote', extra=properties)
 
-            properties = {'custom_dimensions': {'Cats Vote': vote1}}
-            # TODO: use logger object to log cat vote
-            with tracer.span(name=vote1) as span:
-                logger.warning('cat vote', extra=properties)
-
-            properties = {'custom_dimensions': {'Dogs Vote': vote2}}
-            # TODO: use logger object to log dog vote
-            with tracer.span(name=vote2) as span:
-                logger.warning('dog vote', extra=properties)
-
+                if int(vote2) > 0:
+                    properties = {'custom_dimensions': {'Dogs Vote': vote2}}
+                    # TODO: use logger object to log dog vote
+                    with tracer.span(name=vote2) as span:
+                        logger.warning('dog vote', extra=properties)
+            except ValueError as e:
+                logger.error("Error - not integers {0}".format(e))
 
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
@@ -143,6 +145,14 @@ def index():
             # Get current values
             vote1 = r.get(button1).decode('utf-8')
             vote2 = r.get(button2).decode('utf-8')
+
+            tc = TelemetryClient(app.config['INSTRUMENTATION_KEY'])
+            if vote == button1: #button1 = Cats
+                tc.track_event('Cats vote')
+            else: 
+                tc.track_event('Dogs vote')
+
+            tc.flush()
 
             # Return results
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
